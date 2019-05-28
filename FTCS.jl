4c8798025d0b163e4127bcd7523ba6fae3e1423b
Lucas Gabriel Nadolny <44819380+LucasGN1@users.noreply.github.com>
2019-05-27 22:31:34 -0300

    Add files via upload

--- FTCS.jl
+++ FTCS.jl
@@ -1,81 +1,47 @@
-#using Plots
-#gr()
-
-#=
-
-L = 1.0                         # Tamanho da barra
-tmax = 2.0                      # Tempo final
-nx = 100                        # Número de intervalos de x
-nt = 500                        # Número de intervalos de t
-dx = L/nx
-dt = tmax/nt
-α = 1.0e-2                      # Coeficiente de difusão térmica
-r = α*dt/dx^2                   # r < 1/2 para funcionar
-
-if r >= 0.5
-    error("r muito grande")
-end
-
-r2 = 1 - 2*r
-
-d = range(0.0, stop=L, length=(nx+1))
-u = (x -> sin(pi*x)).(d)
-umin = minimum(u)
-umax = maximum(u)
-global t = 0.0
-
-for m = 1:nt
-    plot(d, u, title="t = $(round(t, digits=4))", leg=false)
-    ylims!(umin, umax)
-    savefig("$m")
-    uold = copy(u)
-    global t = t + dt
-    for i = 2:nx
-        u[i] = r*uold[i-1] + r2*uold[i] + r*uold[i+1]
-    end
-end
-
-=#
-
-
-
-
-#=
 using Plots
 gr()
-=#
+
 
 """
     #Inserir explicação
 
 """
-function FTCS(α::Float64, u::Vector;nx = 100, nt = 100, L = 1.0, tmax = 1.0)
+function FTCS(α::Float64, u::Vector, L::Float, tmax::Float; nt = 100)
+    nx = size(u) - 1
     dx = L/nx
     dt = tmax/nt
     r = α*dt/dx^2
-    #=
-    if r >= 0.5
-        error("r = $r >= 1/2, solução instável")
-    end
-    =#
-    r2 = 1 - 2*r
-    #d = range(0.0, stop=L, length=(nx+1))
 
-    #=
+    if r >= 0.5
+        error("r = $r >= 1/2, solução instável.")
+    end
+
+    r2 = 1 - 2*r
+
+    d = range(0.0, stop=L, length=(nx+1))
     umin = minimum(u)
     umax = maximum(u)
-    global t = 0.0
-    =#
+    t = 0.0
+    plot(d, u, label="t = $(round(t, digits=4))")
+    ylims!(umin, umax)
 
     for m = 1:nt
-        #=
-        plot(d, u, title="t = $(round(t, digits=4))", leg=false)
-        ylims!(umin, umax)
-        savefig("$m")
-        =#
         uold = copy(u)
-        #global t = t + dt
+        t = t + dt
         for i = 2:nx
             u[i] = r*uold[i-1] + r2*uold[i] + r*uold[i+1]
         end
+        if t == tmax*(1/4) || t == tmax*(1/2) || t == tmax*(3/4) || t == tmax
+            plot!(d, u, label="t = $(round(t, digits=4))")
+            ylims!(umin, umax)
+        end
     end
+    savefig("Ibagem")
+end
+
+function main()
+    u = (x -> sin(pi*x)).(range(0.0, stop=1.0, length=80))
+    FTCS(1.0, u, 1.0, 1.0)
+end
+
+main()
