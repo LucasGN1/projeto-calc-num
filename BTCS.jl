232679a741b4da9b211c3d13b08e6cf1d1532efd
Lucas Gabriel Nadolny <44819380+LucasGN1@users.noreply.github.com>
2019-05-27 23:31:53 -0300

    Add files via upload

--- BTCS.jl
+++ BTCS.jl
@@ -1,16 +1,19 @@
+using Plots
+gr()
+
 """
-    #Explicação
-    
+    #Inserir explicação
+
 """
-function BTCS(α::Float64, u::Vector; L = 1.0, tmax = 1.0)
-    n = size(Vector)
+function BTCS(α::Float64, u::Vector, L::Float64, tmax::Float64; nt = 100, fig_name="Ibagem")
+    n = size(u)[1]
     A = zeros(n, n)
     dx = L/(n-1)
-    dt = tmax/(n-1)
+    dt = tmax/nt
+
     ai = -α/dx^2
     bi = 1/dt + 2*α/dx^2
     v = [ai bi ai]
-    num = 1/dt
     for i = 2:(n-1)
         for j = 1:3
             A[i, j+i-2] = v[j]
@@ -19,11 +22,39 @@ function BTCS(α::Float64, u::Vector; L = 1.0, tmax = 1.0)
     A[1, 1] = 1.0
     A[n, n] = 1.0
 
-    for t = 1:(n-1)
+    d = range(0.0, stop=L, length=n)
+    umin = minimum(u)
+    umax = maximum(u)
+    t = 0.0
+    plot(d, u, label="t = $(round(t, digits=4))")
+    ylims!(umin, umax)
+    p2, p3 = div(nt, 4), div(nt, 2)
+
+    c = 1/dt
+    for m = 1:nt
         b = copy(u)
         for i = 2:(n-1)
-            b[i] = num*b[i]
+            b[i] = c*b[i]
         end
         u = A\b
-        #Plotar?
+        t = t + dt
+        if m == p2 || m == p3 || m == 3*p2 || m == nt
+            plot!(d, u, label="t = $(round(t, digits=4))")
+            ylims!(umin, umax)
+        end
     end
+    savefig(fig_name)
+end
+
+#=
+
+Exemplo:
+
+function main()
+    u = (x -> sin(pi*x)).(range(0.0, stop=1.0, length=100))
+    BTCS(2.0e-1, u, 1.0, 1.0, fig_name="IbagemBTCS")
+end
+
+main()
+
+=#
